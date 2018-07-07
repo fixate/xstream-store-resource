7919f4570ce8c5664392a4189249ccd8b70133f9
Larry Botha <larry@fixate.it>
2018-07-07 15:06:39 +0200

    chore(tests): create tests file with failing test descriptions

---
+++ __tests__/index.ts
@@ -0,0 +1,17 @@
+describe('xstream-store-resource', () => {
+  test('-> configures request, success, and failure actions', () => {
+    expect(false).toBe(true);
+  });
+
+  test('-> dispatches actions when services respond', () => {
+    expect(false).toBe(true);
+  });
+
+  test('-> accepts a stream creator that extends the resources stream', () => {
+    expect(false).toBe(true);
+  });
+
+  test('-> returns action types that can be subscribed to', () => {
+    expect(false).toBe(true);
+  });
+});
