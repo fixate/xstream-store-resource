0460e89c88e067d872c49fb49f01f67b775f939f
Larry Botha <larry@fixate.it>
2018-07-10 16:44:39 +0200

    chore(actions): add action creators

---
+++ src/action-creators.ts
@@ -0,0 +1,125 @@
+import {IAction} from 'xstream-store';
+
+import {IError, IResource} from './stream-creator-factory';
+
+export interface ID {
+  id: string | number;
+}
+export interface IData {
+  data: {[key: string]: any};
+}
+export interface IParams {
+  params: {[key: string]: any};
+}
+export interface IExtra {
+  [key: string]: any;
+}
+
+const getActions = (actionTypes: any) => ({
+  reset: (): IAction => ({
+    type: actionTypes.RESET,
+  }),
+
+  find: (params?: IParams, extra?: IExtra): IAction => ({
+    ...extra,
+    params,
+    type: actionTypes.FIND,
+  }),
+
+  findSuccess: (items: IResource[]): IAction => ({
+    items,
+    type: actionTypes.FIND_SUCCESS,
+  }),
+
+  findFailure: (lastError: IError): IAction => ({
+    lastError,
+    type: actionTypes.FIND_FAILURE,
+  }),
+
+  get: (id: ID, params?: IParams, extra?: IExtra): IAction => ({
+    ...extra,
+    id,
+    params,
+    type: actionTypes.GET,
+  }),
+
+  getSuccess: (item: IResource): IAction => ({
+    item,
+    type: actionTypes.GET_SUCCESS,
+  }),
+
+  getFailure: (lastError: IError): IAction => ({
+    lastError,
+    type: actionTypes.GET_FAILURE,
+  }),
+
+  create: (data: IData, params: IParams, extra: IExtra): IAction => ({
+    ...extra,
+    data,
+    params,
+    type: actionTypes.CREATE,
+  }),
+
+  createSuccess: (items: IResource[]): IAction => ({
+    items,
+    type: actionTypes.CREATE_SUCCESS,
+  }),
+
+  createFailure: (lastError: IError): IAction => ({
+    lastError,
+    type: actionTypes.CREATE_FAILURE,
+  }),
+
+  patch: (id: ID, data: IData, extra: IExtra): IAction => ({
+    ...extra,
+    data,
+    id,
+    type: actionTypes.PATCH,
+  }),
+
+  patchSuccess: (items: IResource[]): IAction => ({
+    items,
+    type: actionTypes.PATCH_SUCCESS,
+  }),
+
+  patchFailure: (lastError: IError): IAction => ({
+    lastError,
+    type: actionTypes.PATCH_FAILURE,
+  }),
+
+  update: (id: ID, params?: IParams, extra?: IExtra): IAction => ({
+    ...extra,
+    id,
+    params,
+    type: actionTypes.UPDATE,
+  }),
+
+  updateSuccess: (item: IResource): IAction => ({
+    item,
+    type: actionTypes.UPDATE_SUCCESS,
+  }),
+
+  updateFailure: (lastError: IError): IAction => ({
+    lastError,
+    type: actionTypes.UPDATE_FAILURE,
+  }),
+
+  remove: (id: ID, data: IData, extra?: IParams): IAction => ({
+    ...extra,
+    data,
+    id,
+    type: actionTypes.REMOVE,
+  }),
+
+  removeSuccess: (items: IResource): IAction => ({
+    items,
+    type: actionTypes.REMOVE_SUCCESS,
+  }),
+
+  removeFailure: (lastError: IError): IAction => ({
+    lastError,
+    type: actionTypes.REMOVE_FAILURE,
+  }),
+});
+
+export default getActions;
