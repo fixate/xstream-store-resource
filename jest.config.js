e76b6596d5faae997596d75e7cfbd35145b2bb24
Larry Botha <larry@fixate.it>
2018-07-10 16:56:17 +0200

    chore(tests): only run tests with test|spec

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   transform: {
     '^.+\\.(j|t)sx?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testRegex: '(/__tests__/.*(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
