055bb8e06f12e5ea19fa365a8adbc6936b90f339
Larry Botha <larry@fixate.it>
2018-07-10 16:50:27 +0200

    chore(createStreamCreator): added module for creating stream creators

--- src/stream-creator-factory.ts
+++ src/stream-creator-factory.ts
@@ -1,21 +1,14 @@
-// create action creators using name
-// create a stream
-//  accepting an endpoint
-//  and a base effect creator
-//  and methods to subscribe to
-//  and an additional custom effect creator that can react to actions, and is added to the effect creator
-//  building an effect creator for only those methods
-//  returning an object with the stream, effect creator, and request enums
-//  return action creators and types
+import xs from 'xstream';
+import {IAction, IScopedState, IStreamCreator} from 'xstream-store';
 
-enum RequestStates {
+export enum RequestStates {
   IDLE,
   REQUESTING,
   SUCCESS,
   FAILURE,
 }
 
-enum RequestMethods {
+export enum RequestMethods {
   IDLE,
   GETTING,
   FINDING,
@@ -25,14 +18,97 @@ enum RequestMethods {
   REMOVING,
 }
 
-interface IResource {
+export interface IResource {
   [key: string]: any;
 }
-
-interface IResourceState {
-  item: IResource;
-  items: [IResource];
-  lastError: ErrorEvent;
+export interface IError {
+  [key: string]: any;
+}
+export interface IResourceState {
+  entity: IResource | null;
+  items: IResource[];
+  lastError: IError;
   requestMethod: RequestMethods;
   requestState: RequestStates;
 }
+
+const initialState: IResourceState = {
+  entity: null,
+  items: [],
+  lastError: {},
+  requestMethod: RequestMethods.IDLE,
+  requestState: RequestStates.IDLE,
+};
+
+const createStreamCreator: (actionTs: {[key: string]: string}) => IStreamCreator = actionTypes => {
+  return select =>
+    xs
+      .merge(
+        xs
+          .merge(
+            select(actionTypes.CREATE_FAILURE),
+            select(actionTypes.FIND_FAILURE),
+            select(actionTypes.GET_FAILURE),
+            select(actionTypes.PATCH_FAILURE),
+            select(actionTypes.REMOVE_FAILURE),
+            select(actionTypes.UPDATE_FAILURE),
+          )
+          .map(action => (state: IScopedState) => ({
+            ...state,
+            lastError: action.lastError,
+            requestMethod: RequestMethods.IDLE,
+            requestState: RequestStates.FAILURE,
+          })),
+
+        select(actionTypes.FIND).map(_ => (state: IScopedState) => ({
+          ...state,
+          requestMethod: RequestMethods.FINDING,
+          requestState: RequestStates.REQUESTING,
+        })),
+
+        select(actionTypes.GET).map(_ => (state: IScopedState) => ({
+          ...state,
+          requestMethod: RequestMethods.GETTING,
+          requestState: RequestStates.REQUESTING,
+        })),
+
+        select(actionTypes.PATCH).map(_ => (state: IScopedState) => ({
+          ...state,
+          requestMethod: RequestMethods.PATCHING,
+          requestState: RequestStates.REQUESTING,
+        })),
+
+        select(actionTypes.UPDATE).map(_ => (state: IScopedState) => ({
+          ...state,
+          requestMethod: RequestMethods.UPDATING,
+          requestState: RequestStates.REQUESTING,
+        })),
+
+        xs
+          .merge(
+            select(actionTypes.CREATE_SUCCESS),
+            select(actionTypes.GET_SUCCESS),
+            select(actionTypes.PATCH_SUCCESS),
+            select(actionTypes.REMOVE_SUCCESS),
+            select(actionTypes.UPDATE_SUCCESS),
+          )
+          .map(action => (state: IScopedState) => ({
+            ...state,
+            item: action.item,
+            requestMethod: RequestMethods.IDLE,
+            requestState: RequestStates.SUCCESS,
+          })),
+
+        select(actionTypes.FIND_SUCCESS).map(action => (state: IScopedState) => ({
+          ...state,
+          items: action.items,
+          requestMethod: RequestMethods.IDLE,
+          requestState: RequestStates.SUCCESS,
+        })),
+
+        select(actionTypes.RESET).map(_ => (_: any) => initialState),
+      )
+      .startWith(() => initialState);
+};
+
+export default createStreamCreator;
