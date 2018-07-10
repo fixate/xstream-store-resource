c4f110225bc57f2c6695e05f6d7994eb2fd7b79f
Larry Botha <larry@fixate.it>
2018-07-10 18:34:18 +0200

    test(stream creator): test request states and methods

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,21 +1,79 @@
-import createActionTypes from '../src/action-types';
+import xs from 'xstream';
+import buffer from 'xstream/extra/buffer';
+import createStore from 'xstream-store';
+
+import {RequestMethods, RequestStates} from '../src/stream-creator-factory';
 import createResource from '../src/index';
 
-describe('xstream-store-resource', () => {
-  test('-> throws if initialised without name', () => {
-    expect(() => createResource({name: '', url: 'fakeurl'})).toThrow();
-  });
+const getResource = (options = {}) => {
+  const defaultOptions = {
+    name: 'my-service',
+    url: 'fakeurl',
+  };
 
-  test('-> throws if initialised without url', () => {
+  return createResource({...defaultOptions, ...options});
+};
+
+describe('xstream-store-resource', () => {
+  test('-> throws if initialised without required params', () => {
+    expect(() => createResource({name: '', url: 'fakeurl'})).toThrow();
     expect(() => createResource({name: 'fakename', url: ''})).toThrow();
   });
 
-  test('-> configures request, success, and failure actions', () => {
-    expect(false).toBe(true);
+  test('-> request state is properly set when requests are made', () => {
+    const resource = getResource();
+    const store = createStore({myResource: resource.streamCreator});
+
+    ['create', 'find', 'get', 'patch', 'update', 'remove'].map(actionName => {
+      const sub = store.state$
+        .map(({myResource}) => ({...myResource}))
+        .last()
+        .subscribe({
+          next(res: any) {
+            expect(res.requestState).toBe(RequestStates.REQUESTING);
+            expect(res.requestMethod).not.toBe(RequestMethods.IDLE);
+          },
+        });
+
+      store.dispatch(resource.actions[actionName]());
+      store.state$.shamefullySendComplete();
+
+      sub.unsubscribe();
+    });
   });
 
-  test('-> dispatches actions when services respond', () => {
-    expect(false).toBe(true);
+  test('-> sets request state and methods on successful responses', () => {
+    const resource = getResource();
+    const store = createStore({myResource: resource.streamCreator}, [...resource.effectCreators]);
+
+    ['create', 'find', 'get', 'patch', 'update', 'remove'].map(actionName => {
+      const response = {someProp: actionName};
+      const spy = jest.spyOn(resource.actions, `${actionName}Success`);
+      fetch.mockResponse(JSON.stringify(response));
+
+      const sub = store.state$
+        .map(({myResource}) => ({...myResource}))
+        .compose(buffer(xs.never()))
+        .subscribe({
+          next(res: any) {
+            if (actionName === 'find') {
+              expect(res.items).toContain(response);
+            } else {
+              expect(res.entity).toBe(response);
+            }
+
+            expect(res.requestState).toBe(RequestStates.SUCCESS);
+            expect(res.requestMethod).toBe(RequestStates.IDLE);
+            expect(spy).toHaveBeenCalledTimes(1);
+            expect(spy).toHaveBeenCalledWith(response);
+          },
+        });
+
+      store.dispatch(resource.actions[actionName]());
+      store.state$.shamefullySendComplete();
+
+      sub.unsubscribe();
+    });
   });
 
   test('-> handles endpoints with multiple parameters', () => {
