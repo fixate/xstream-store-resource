6b950c8bab60363851991a173873c793300f9b64
Larry Botha <larry@fixate.it>
2018-07-11 11:44:38 +0200

    fix(fetch provider): add missing params from function call

--- src/providers.ts
+++ src/providers.ts
@@ -1,5 +1,11 @@
-const fetchProvider = (url: string, requestConfig: object) => {
-  return fetch(url, requestConfig).then((res: any) => {
+export type Provider = (
+  url: string,
+  data: {[key: string]: any},
+  requestConfig: {[key: string]: any},
+) => Promise<Response>;
+
+export const fetchProvider: Provider = (url: string, data, requestConfig: object) => {
+  return fetch(url, {...requestConfig, body: JSON.stringify(data)}).then((res: any) => {
     if (!res.ok) {
       throw res.json();
     }
@@ -7,5 +13,3 @@ const fetchProvider = (url: string, requestConfig: object) => {
     return res.json();
   });
 };
-
-export {fetchProvider};
